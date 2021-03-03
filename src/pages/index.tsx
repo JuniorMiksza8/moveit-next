16e7692a88203d0e3f6e91c3d1a5db8ea3e03c18
Junior Miksza <junior.miksza8@gmail.com>
2021-03-03 07:06:03 -0300

    Countdown
    Countdown component created

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,23 @@
+import CompletedChallenges from "../components/CompletedChallenges";
+import Countdown from "../components/Countdown";
 import ExperinceBar from "../components/ExperinceBar";
 import Profile from "../components/Profile";
 import styles from '../styles/pages/home.module.css'
 
+import Head from 'next/head'
+
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Inicio move.it</title>
+      </Head>
       <ExperinceBar />
       <section>
         <div>
           <Profile />
+          <CompletedChallenges />
+          <Countdown />
         </div>
         <div>
 
